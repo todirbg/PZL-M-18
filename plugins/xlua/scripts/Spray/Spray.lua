d70a4dcddd5fde1c2bb6f1bb1c12f0f9153c6b29
todirbg <todirbg@yahoo.com>
2021-03-05 00:37:33 +0200

    Continue working on spray

--- plugins/xlua/scripts/Spray/Spray.lua
+++ plugins/xlua/scripts/Spray/Spray.lua
@@ -7,6 +7,7 @@ air_speed = find_dataref("sim/flightmodel/forces/vz_air_on_acf")
 prop_wash = find_dataref("sim/flightmodel2/engines/jetwash_mtr_sec[0]")
 atom_prop_deg = create_dataref("custom/dromader/spray/atom_prop_ang","number", dummy)
 pump_prop_deg = create_dataref("custom/dromader/spray/pump_prop_ang","number", dummy)
+pump_prop_deg_sec = create_dataref("custom/dromader/spray/pump_prop_deg_sec","number", dummy)
 boom_press = create_dataref("custom/dromader/spray/boom_press","number", dummy)
 boom_hide = create_dataref("custom/dromader/spray/boom_hide","number", dummy)
 boom_fuse = create_dataref("custom/dromader/spray/boom_fuse","number", dummy)
@@ -49,15 +50,26 @@ function after_physics()
 		
 		if boom_fuse == 1 then
 			local temp_pump_deg = pump_prop_deg
-			temp_pump_deg = temp_pump_deg + math.max(0,(air_speed +  prop_wash) )*36*SIM_PERIOD
+			temp_pump_deg = temp_pump_deg + math.max(0,(air_speed +  prop_wash/2) )*36*SIM_PERIOD
+			pump_prop_deg_sec = (temp_pump_deg - pump_prop_deg)/(SIM_PERIOD*60)
+			boom_press = math.min(1.5, math.max(0, pump_prop_deg_sec/30) )
+			if spray == 1 then
+				if boom_press > 0.5 then
+					boom_press = boom_press - 0.5
+				end
+			end
 			if temp_pump_deg > 360 then
 				temp_pump_deg = temp_pump_deg - 360
 			end	
 			pump_prop_deg = temp_pump_deg
 		end	
 		
-		if spray == 1 and water_quantity > 0 and boom_fuse == 1 then
-			water_quantity = water_quantity - 20*SIM_PERIOD
+		if water_quantity > 0 and boom_fuse == 1 then
+			if spray == 1 then
+				water_quantity = water_quantity - 10*boom_press*SIM_PERIOD
+			end
+		else
+			spray = 0
 		end
 	end
 end
